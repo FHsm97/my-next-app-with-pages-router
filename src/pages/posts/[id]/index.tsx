de9042d18ee508b3f669e5d1b3f59d6882c0da59
FHsm97 <hashemi.fatemeh1379@gmail.com>
2025-09-04 13:15:07 +0330

    S3-E11-ssr

--- src/pages/posts/[id]/index.tsx
+++ src/pages/posts/[id]/index.tsx
@@ -1,8 +1,8 @@
-
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router"
 
 // export default function PostSinglePage({params}:{params:any}) {
-
-import { useRouter } from "next/router"
 
 //     console.log(params)
 //     return(
@@ -10,16 +10,55 @@ import { useRouter } from "next/router"
 //     )
 // }   return undefined
 
-export default function PostSinglePage() {
+type Post={
+    id:number
+    userId:number
+    title:string
+    body:string
+}
 
-    const router=useRouter()
+export default function PostSinglePage({post}:{post:Post}) {
+
+    // const router=useRouter()
 
     // console.log(router.query)
 
-    const id=router.query?.id;
+    // const id=router.query?.id;
 
     
     return(
-        <h1>Post :{id}</h1>
+        <>
+        <h1>Post :{post.title}</h1>
+
+        <Link href='/posts'>Back To Post List</Link>
+        
+        </>
     )
+}
+
+//server side 
+export const getServerSideProps:GetServerSideProps=async({params,req})=>{
+    let res=await fetch(`https://jsonplaceholder.typicode.com/posts/${params?.id}`)
+
+
+    // console.log(req.cookies)
+
+
+    if (!res.ok) {
+        // if (res.status==404) {
+        //     return notFound(); 
+        // }
+        throw new Error('somthing went wrong!')
+        
+    }
+
+    let post= await res.json();
+
+
+    return{
+        props:{
+            post
+        }
+    }
+
 }
