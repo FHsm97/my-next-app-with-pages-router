149d6f85be7597391535b89dccfa24f1aa62a6e5
FHsm97 <hashemi.fatemeh1379@gmail.com>
2025-09-06 15:39:58 +0330

    S4-E5-progress bar(next-progress)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,33 @@
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
- 
+// import { PagesProgressBar } from 'next-nprogress-bar'
+// import { PagesProgressBar as ProgressBar } from 'next-nprogress-bar';
+import NextProgress from "next-progress";
+
+
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
- 
+
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
- 
+
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
- 
-  return getLayout(<Component {...pageProps} />)
+
+  return getLayout(
+    <>
+      <NextProgress delay={300} options={{ showSpinner: false }} />
+      <Component {...pageProps} />
+      {/* <ProgressBar
+        height="4px"
+        color="#fffd00"
+        options={{ showSpinner: false }}
+        shallowRouting
+      /> */}
+    </>
+  )
 }
